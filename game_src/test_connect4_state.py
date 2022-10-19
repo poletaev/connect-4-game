133dbe30f331ed01b5cd0b914f570e9aa0cbb3d7
Andrey Poletaev <poletaev.andrew@gmail.com>
2022-10-19 16:29:50 +0600

    Add unit-test for horizontal winning

---
+++ game_src/test_connect4_state.py
@@ -0,0 +1,22 @@
+import unittest
+
+from connect4_game import Connect4State
+
+class TestHorizontalWinning(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.state = Connect4State()
+        self.state.grid = [[1, 1, 1, 1, 0, 0],
+                            [0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0]
+                            ]
+
+    def test_player1_won(self):
+        self.assertTrue(self.state.is_game_over())
+        self.assertEqual(self.state.player_won, 1)
+
+if __name__ == '__main__':
+    unittest.main()
